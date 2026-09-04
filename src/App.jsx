e96f2cbc30218455a3179322d59b8ff8387b6da5
George Prielipp <goaliegeo30@gmail.com>
2026-09-04 11:03:35 -0500

    Added necessary technologies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">

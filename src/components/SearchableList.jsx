6de0311a279d70b081ec1110b32c9685f21905f3
George Prielipp <goaliegeo30@gmail.com>
2026-09-06 11:04:10 -0500

    stubs

---
+++ src/components/SearchableList.jsx
@@ -0,0 +1,8 @@
+export default function SearchableList ({ view }) {
+    return (
+        <>
+            <h1>Search bar :D</h1>
+            <view />
+        </>
+    )
+}
---
+++ src/components/StudySetList.jsx
@@ -0,0 +1,7 @@
+export default function StudySetList () {
+    return (
+        <>
+            <p>List of Study Sets :)</p>
+        </>
+    );
+}

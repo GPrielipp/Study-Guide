40528644397cdd745a1f637a3fb97cd12740ede0
George Prielipp <goaliegeo30@gmail.com>
2026-09-06 10:59:30 -0500

    stubbed page not found / future updates coming.

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,8 @@
+export default function PageNotFound () {
+    return (
+        <>
+            <h1>This feature is not supported yet.</h1>
+            <p>Keep your eyes peeled for future updates!</p>
+        </>
+    );
+}
